70aab7c3f88a03347113ddf7dd8a75606c5b6efd
MaxSabirov <53007578+MaxSabirov@users.noreply.github.com>
2021-02-02 03:20:26 +0200

    nfg

--- Puzzle Pairs/Assets/Scripts/ScenesManager.cs
+++ Puzzle Pairs/Assets/Scripts/ScenesManager.cs
@@ -9,15 +9,15 @@ public class ScenesManager : MonoBehaviour
     [SerializeField] Text levelNumberText;
     [SerializeField] Text playerActionsText;
     [SerializeField] Text _levelNumberText;
-    [SerializeField] Text _timerText;
+    //[SerializeField] Text _timerText;
     [SerializeField] Text _actionText;
-    [SerializeField] Text timerText;
+    //[SerializeField] Text timerText;
 
     public static int levelsNow=0;
     private int playerActions;   
 
     public bool ifWin;
-    public float time;
+    //public float time;
     public int[] nextSectionLevel;
     int section;
     
@@ -33,25 +33,25 @@ public class ScenesManager : MonoBehaviour
         BlackBoard.scenesManager = this;
         levels[levelsNow-1].SetActive(true);
         levelNumberText.text = "Level "+ (levelsNow);
+        levelsNow--;
         winPanel.SetActive(false);
     }
     void Update()
     {
-        
         if (!ifWin)
         {
-            time = time + Time.deltaTime;
-            int minutes = Mathf.FloorToInt(time / 60);
-            int seconds = Mathf.FloorToInt(time - minutes * 60f);
-            string textTime = string.Format("{0:0}:{1:00}", minutes, seconds);
-            timerText.text = textTime;
+            //time = time + Time.deltaTime;
+            //int minutes = Mathf.FloorToInt(time / 60);
+            //int seconds = Mathf.FloorToInt(time - minutes * 60f);
+            //string textTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+            //timerText.text = textTime;
         }
         else
         {
             winPanel.SetActive(true);
             _levelNumberText.text = levelNumberText.text+ " Completed";
             _actionText.text = "You Did "+ playerActionsText.text + " Moves";
-            _timerText.text = "Your Time "+timerText.text;
+            //_timerText.text = "Your Time "+timerText.text;
 
         }
     }
@@ -59,7 +59,7 @@ public class ScenesManager : MonoBehaviour
     public void RestartLevel()
     {
         playerActions = 0;
-        time = 0;
+        //time = 0;
         ifWin = false;
         playerActionsText.text = "Moves: " + playerActions.ToString();
         BlackBoard.curser.RestartLevel();
@@ -82,34 +82,31 @@ public class ScenesManager : MonoBehaviour
     }
     public void ExitLevel()
     {
-        levels[levelsNow - 1].SetActive(false);
         SceneManager.LoadScene("Main Menu");
     }
-    public void NextLevel()
+
+
+    public void Next_PreviousLevel(bool isNext)
     {
         RestartLevel();
         levels[levelsNow].SetActive(false);
-        levelsNow++;
-        if (levelsNow + 1 > levels.Length)
+        if (isNext)
         {
-            levelsNow--; 
+            levelsNow++;
+            if (levelsNow + 1 > levels.Length)
+            {
+                levelsNow--;
+            }
+            winPanel.SetActive(false);
         }
-        levels[levelsNow].SetActive(true);
-        levelNumberText.text = "Level " + (levelsNow + 1);
-        winPanel.SetActive(false);
-        BlackBoard.curser.RestartLevel();
-    }
-    public void PreviousLevel()
-    {
-        RestartLevel();
-        
-        levels[levelsNow].SetActive(false);
-        levelsNow--;
-        if (levelsNow < 0)
+        else
         {
-            levelsNow = 0;
-            
-        }
+            levelsNow--;
+            if (levelsNow < 0)
+            {
+                levelsNow = 0;
+            }
+        }        
         levels[levelsNow].SetActive(true);
         levelNumberText.text = "Level " + (levelsNow + 1);
         BlackBoard.curser.RestartLevel();
@@ -120,38 +117,67 @@ public class ScenesManager : MonoBehaviour
         playerActions++;
         playerActionsText.text = "Moves: " + playerActions.ToString();
     }
-    public void NextSection()
-    {
-        section++;
-        if (section >= nextSectionLevel.Length)
-        {
-            section--;
-        }
-        else
-        {
-            RestartLevel();
-            levels[levelsNow].SetActive(false);
-            levelsNow = nextSectionLevel[section] - 1;
-            levels[nextSectionLevel[section]-1].SetActive(true);
-            levelNumberText.text = "Level " + nextSectionLevel[section];
-            BlackBoard.curser.RestartLevel();
-        }
-    }
-    public void PreviousSection()
-    {
-        section--;
-        if (section<0)
-        {
-            section++;
-        }
-        else
-        {
-            RestartLevel();
-            levels[levelsNow].SetActive(false);
-            levelsNow = nextSectionLevel[section] - 1;
-            levels[nextSectionLevel[section]-1].SetActive(true);
-            levelNumberText.text = "Level " + nextSectionLevel[section];
-            BlackBoard.curser.RestartLevel();
-        }
-    }
+
+    //public void NextLevel()
+    //{
+    //    RestartLevel();
+    //    levels[levelsNow].SetActive(false);
+    //    levelsNow++;
+    //    if (levelsNow + 1 > levels.Length)
+    //    {
+    //        levelsNow--; 
+    //    }
+    //    levels[levelsNow].SetActive(true);
+    //    levelNumberText.text = "Level " + (levelsNow + 1);
+    //    winPanel.SetActive(false);
+    //    BlackBoard.curser.RestartLevel();
+    //}
+    //public void PreviousLevel()
+    //{
+    //    RestartLevel();
+    //    levels[levelsNow].SetActive(false);
+    //    levelsNow--;
+    //    if (levelsNow < 0)
+    //    {
+    //        levelsNow = 0;
+
+    //    }
+    //    levels[levelsNow].SetActive(true);
+    //    levelNumberText.text = "Level " + (levelsNow + 1);
+    //    BlackBoard.curser.RestartLevel();
+    //}
+    //public void NextSection()
+    //{
+    //    section++;
+    //    if (section >= nextSectionLevel.Length)
+    //    {
+    //        section--;
+    //    }
+    //    else
+    //    {
+    //        RestartLevel();
+    //        levels[levelsNow].SetActive(false);
+    //        levelsNow = nextSectionLevel[section] - 1;
+    //        levels[nextSectionLevel[section]-1].SetActive(true);
+    //        levelNumberText.text = "Level " + nextSectionLevel[section];
+    //        BlackBoard.curser.RestartLevel();
+    //    }
+    //}
+    //public void PreviousSection()
+    //{
+    //    section--;
+    //    if (section<0)
+    //    {
+    //        section++;
+    //    }
+    //    else
+    //    {
+    //        RestartLevel();
+    //        levels[levelsNow].SetActive(false);
+    //        levelsNow = nextSectionLevel[section] - 1;
+    //        levels[nextSectionLevel[section]-1].SetActive(true);
+    //        levelNumberText.text = "Level " + nextSectionLevel[section];
+    //        BlackBoard.curser.RestartLevel();
+    //    }
+    //}
 }
